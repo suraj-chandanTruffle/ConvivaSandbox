f6b06c8fff003ab41ee54514c2265e3e6f672143
suraj-chandanTruffle <suraj.chandan@trufflecorp.com>
2025-12-10 15:18:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function initEmbeddedMessaging() {
+    try {
+        embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+        embeddedservice_bootstrap.init(
+            '00DVA000008Fuc9',
+            'Conviva_Messaging_Channel',
+            'https://convivareimagined--partialsb.sandbox.my.site.com/ESWConvivaMessagingChan1765182896774',
+            {
+                scrt2URL: 'https://convivareimagined--partialsb.sandbox.my.salesforce-scrt.com'
+            }
+        );
+    } catch (err) {
+        console.error('Error loading Embedded Messaging: ', err);
+    }
+};
+
 function greet() {
     alert("Hello from JavaScript!");
 }
