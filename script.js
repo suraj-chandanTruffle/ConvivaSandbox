136a617acf0549ed9fe6d32a35726cc9aafc06eb
pjindal13 <pranika.jindal@trufflecorp.com>
2025-12-30 13:00:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let message = '';
 let isChatOpened = false;
+let botloaded = false;
+let botreplied = false;
+let messageSentCount = 0;
+
 document.getElementById("convivaChatInput").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
         message = e.target.value;
@@ -9,17 +13,49 @@ document.getElementById("convivaChatInput").addEventListener("keydown", function
         }
     }
 });
+
+function pushPrechatFields() {
+    try {
+      if (window.embeddedservice_bootstrap?.prechatAPI?.setHiddenPrechatFields) {
+       
+        const preChatFields = {"url": window.location.href}
+         console.log("📨 Prechat fields sent:", preChatFields);
+        embeddedservice_bootstrap.prechatAPI.setHiddenPrechatFields(preChatFields);
+      }
+    } catch (e) {
+      console.warn("Prechat push failed:", e);
+    }
+}
+
+
 function initEmbeddedMessaging() {
     try {
+        
         embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
         embeddedservice_bootstrap.init(
             '00DVA000008Fuc9',
-            'Conviva_Messaging_Channel',
-            'https://convivareimagined--partialsb.sandbox.my.site.com/ESWConvivaMessagingChan1765182896774',
+            'ConvivaSandeepDemo',
+            'https://convivareimagined--partialsb.sandbox.my.site.com/ESWConvivaSandeepDemo1766433506241',
             {
                 scrt2URL: 'https://convivareimagined--partialsb.sandbox.my.salesforce-scrt.com'
             }
         );
+        window.addEventListener('onEmbeddedMessageSent', function (event) {
+            console.log("✅ onEmbeddedMessageSent");
+            messageSentCount++;
+             
+            if(messageSentCount>=2){
+               botreplied = true;
+                const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
+                chatWindow.classList.add("hideContainer");
+                chatWindow.classList.remove("unhideContainer");
+            }
+        });
+          window.addEventListener('onEmbeddedMessagingConversationParticipantChanged', function (event) {
+            console.log("✅ onEmbeddedMessagingConversationParticipantChanged");
+              botloaded =true;
+             
+        });
         window.addEventListener("onEmbeddedMessagingReady", () => {
             console.log("✅ onEmbeddedMessagingReady");
             setTimeout(() => {
@@ -29,12 +65,13 @@ function initEmbeddedMessaging() {
                     chatBubble.classList.remove("hideContainer");
                 }
             }, 1500);
+            pushPrechatFields();
         });
         window.addEventListener('onEmbeddedMessagingConversationStarted', function (event) {
             console.log("✅ onEmbeddedMessagingConversationStarted");
-            const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
-            chatWindow.classList.add("hideContainer");
-            chatWindow.classList.remove("unhideContainer");
+           // const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
+           // chatWindow.classList.add("hideContainer");
+            //chatWindow.classList.remove("unhideContainer");
         });
         window.addEventListener('onEmbeddedMessagingFirstBotMessageSent', function (event) {
             console.log("✅ onEmbeddedMessagingFirstBotMessageSent");
@@ -44,12 +81,20 @@ function initEmbeddedMessaging() {
         window.addEventListener('onEmbeddedMessagingConversationOpened', function (event) {
             console.log("✅ onEmbeddedMessagingConversationOpened");
             isChatOpened = true;
-            const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
-            chatBubble.classList.add("hideContainer");
-            chatBubble.classList.remove("unhideContainer");
+            //const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
+            //chatBubble.classList.add("hideContainer");
+            //chatBubble.classList.remove("unhideContainer");
         });
         window.addEventListener('onEmbeddedMessagingWindowClosed', function (event) {
             isChatOpened = false;
+            botloaded = false;
+            botreplied = false;
+            messageSentCount = 0;
+             const quickChatContainer = document.querySelector('[data-id="quickChatContainer"]');
+            quickChatContainer.classList.remove("hideQuickContainer");
+            const quickChatLoadContainer = document.querySelector('[data-id="quickChatLoadContainer"]');
+            quickChatLoadContainer.classList.add("hideQuickContainer");
+            
         });
     } catch (err) {
         console.error('Error loading Embedded Messaging: ', err);
@@ -88,5 +133,11 @@ function onINeedTechnicalSupport(){
     doCallAgentforce();
 }
 function doCallAgentforce() {
+            const quickChatContainer = document.querySelector('[data-id="quickChatContainer"]');
+            quickChatContainer.classList.add("hideQuickContainer");
+            const quickChatLoadContainer = document.querySelector('[data-id="quickChatLoadContainer"]');
+            quickChatLoadContainer.classList.remove("hideQuickContainer");
+
+    
     embeddedservice_bootstrap.utilAPI.launchChat();
 }
